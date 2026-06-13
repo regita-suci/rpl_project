624e745d0ccc5fc55399f995433c3a43ac94f37d
Regita <regitaa.suci@gmail.com>
2026-06-13 20:24:53 +0700

    Menambahkan fitur CRUD mahasiswa

--- mahasiswa/admin.py
+++ mahasiswa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Mahasiswa
 
-# Register your models here.
+@admin.register(Mahasiswa)
+class MahasiswaAdmin(admin.ModelAdmin):
+    list_display = ('nim', 'nama', 'programstudi', 'angkatan')
+    search_fields = ('nim', 'nama')
+    list_filter = ('programstudi', 'angkatan')
--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mahasiswa
-from django.contrib.auth.decorators import login_required  # ← tambahkan import ini
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     context = {
@@ -9,7 +10,54 @@ def index(request):
     }
     return render(request, 'mahasiswa/index.html', context)
 
-@login_required(login_url='/accounts/login/')  # ← tambahkan decorator ini
+
+@login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
     return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
+
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+
+    if request.method == 'POST':
+        Mahasiswa.objects.create(
+            nim=request.POST['nim'],
+            nama=request.POST['nama'],
+            programstudi=request.POST['programstudi'],
+            angkatan=request.POST['angkatan']
+        )
+
+        return redirect('daftar_mahasiswa')
+
+    return render(request, 'mahasiswa/form.html')
+
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, id):
+
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+
+    if request.method == 'POST':
+        mahasiswa.nim = request.POST['nim']
+        mahasiswa.nama = request.POST['nama']
+        mahasiswa.programstudi = request.POST['programstudi']
+        mahasiswa.angkatan = request.POST['angkatan']
+        mahasiswa.save()
+
+        return redirect('daftar_mahasiswa')
+
+    return render(
+        request,
+        'mahasiswa/form.html',
+        {'mahasiswa': mahasiswa}
+    )
+
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, id):
+
+    mahasiswa = get_object_or_404(Mahasiswa, id=id)
+    mahasiswa.delete()
+
+    return redirect('daftar_mahasiswa')
